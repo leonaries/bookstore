75fbd73c8854932de1878f528e432306dec91e9b
leonaries <18382336395@163.com>
2018-12-04 18:06:44 +0800

    promise合并

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -21,29 +21,43 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      wx.showLoading({
+        title: '载入中',
+      })
       const bid = options.bid
       const detail = bookModel.getDetail(bid)
       const comments = bookModel.getComments(bid)
       const likeStatus = bookModel.getLikeStatus(bid)
-      
-      detail.then(res =>{
-          this.setData({
-              book:res
-          })
+
+      //并行请求  Promise合并
+      Promise.all([detail, comments, likeStatus])
+      .then(res =>{
+        this.setData({
+          book:res[0],
+          comments: res[1].comments,
+          likeStatus:res[2].like_status,
+          likeCount:res[2].fav_nums
+        })
+        wx.hideLoading()
       })
+      // detail.then(res =>{
+      //     this.setData({
+      //         book:res
+      //     })
+      // })
       
-      comments.then(res =>{
-          this.setData({
-              comments:res.comments
-          })
-      })
+      // comments.then(res =>{
+      //     this.setData({
+      //         comments:res.comments
+      //     })
+      // })
       
-      likeStatus.then(res =>{
-          this.setData({
-              likeStatus:res.like_status,
-              likeCount:res.fav_nums
-          })
-      })
+      // likeStatus.then(res =>{
+      //     this.setData({
+      //         likeStatus:res.like_status,
+      //         likeCount:res.fav_nums
+      //     })
+      // })
   },
 
   onLike(e){
