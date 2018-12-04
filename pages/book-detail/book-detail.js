23abb2c249808dae0aec9c054ec16bf15b3d25d2
leonaries <18382336395@163.com>
2018-12-04 17:37:32 +0800

    短评模块编写完毕

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,21 +1,94 @@
 // pages/book-detail/book-detail.js
+import {BookModel} from  '../../models/book'
+import {LikeModel} from '../../models/like.js'
+const bookModel = new BookModel()
+const likeModel = new LikeModel()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      comments:[],
+      book:null,
+      likeStatus:false,
+      likeCount:0,
+      posting:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const bid = options.bid
-    console.log(bid)
+      const bid = options.bid
+      const detail = bookModel.getDetail(bid)
+      const comments = bookModel.getComments(bid)
+      const likeStatus = bookModel.getLikeStatus(bid)
+      
+      detail.then(res =>{
+          this.setData({
+              book:res
+          })
+      })
+      
+      comments.then(res =>{
+          this.setData({
+              comments:res.comments
+          })
+      })
+      
+      likeStatus.then(res =>{
+          this.setData({
+              likeStatus:res.like_status,
+              likeCount:res.fav_nums
+          })
+      })
   },
 
+  onLike(e){
+    const behavior = e.detail.behavior
+    likeModel.like(behavior,this.data.book.id,400)
+  },
+
+  onFakePost(e){
+      this.setData({
+        posting:true
+      })
+  },
+  onCancel(e){
+    this.setData({
+      posting: false
+    })
+  },
+  onPost(e){
+    const comment = e.detail.text || e.detail.value
+    //避免用户输入空数据
+    if(!comment){
+      return
+    }
+    if(comment.length > 12){
+        wx.showToast({
+          title: '短评最多12个字',
+          icon:'none'
+        })
+        return
+    }
+    bookModel.postComment(this.data.book.id,comment).then(res =>{
+      wx.showToast({
+        title: '+1',
+        icon:'none'
+      })
+      this.data.comments.unshift({
+        content:comment,
+        nums:1
+      })
+      this.setData({
+        comments:this.data.comments,
+        posting:false
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
